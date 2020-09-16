12118c1bf439a5616050f99e8d04241120288eed
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2020-09-16 13:55:53 +0530

    Removed video streaming feature due to bugs

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,30 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import cv2
-from imutils.video import WebcamVideoStream
 
-app =  Flask(__name__,template_folder='template')
-app.config['SECRET_KEY'] = #Enter a secret key here
+app = Flask(__name__, template_folder='template')
+app.config['SECRET_KEY'] = 'd217925160d31eeb1d02a37a6de67e6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view ='login'
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
-class VideoCamera(object):
-    def __init__(self):
-        self.stream = WebcamVideoStream(src=1).start()
-
-    def __del__(self):
-        self.stream.stop()
-
-    def get_frame(self):
-        image = self.stream.read()
-        ret, jpeg = cv2.imencode('.jpg', image)
-        data = []
-        data.append(jpeg.tobytes())
-        return data
 
 from flaskblog import routes
